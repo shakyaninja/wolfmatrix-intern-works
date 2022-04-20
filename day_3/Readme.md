dfc0f17d171c6e0bf9f90418b0638661ad7e2105
shakya445 <shakya445@gmail.com>
2022-04-20 15:14:58 +0545

    adds JS car parking cost challenge

---
+++ day_3/Readme.md
@@ -0,0 +1,46 @@
+Day 3 (2079-01-07 || Wednesday) 
+---
+# Core PHP (Hypertext Preprocessor)
+
+## Intro
+* PHP is an acronym for "PHP: Hypertext Preprocessor"
+* PHP is a widely-used, open source scripting language
+* PHP scripts are executed on the server
+* PHP is free to download and use
+* PHP can generate dynamic page content
+* PHP can create, open, read, write, delete, and close files on the server
+* PHP can collect form data
+* PHP can send and receive cookies
+* PHP can add, delete, modify data in your database
+* PHP can be used to control user-access
+* PHP can encrypt data
+
+## Installation
+
+## Syntax
+## Variables
+## Data Types
+## Constants
+## Operators
+## Loops
+## Functions
+## Arrays
+## Superglobals
+## RegEx
+## Forms
+## File Handling
+## Session
+## Cookies
+## PHP Callback functions
+## PHP Filters
+## PHP JSON
+## PHP Exceptions
+## PHP OOP
+## PHP Classes/Objects
+## PHP Constructors
+## PHP Destructors
+## PHP Interfaces
+## PHP Namespace
+## PHP Iterables
+
+# MySQL Database Basics
---
+++ day_3/main.js
@@ -0,0 +1,108 @@
+var entryTime = ''; //HH:MM format
+var leaveTime = ''; //HH:MM format
+
+var cost = 0;
+
+const addEntranceFee = () => {
+    cost = cost + 2;
+    console.log("Entrance Fee: +2");
+}
+
+const addPartialHrFee = () => {
+    cost = cost + 3;
+    console.log("First Partial or Full Hour Fee: +3");
+} 
+
+const addFullHrFee = () => {
+    cost = cost + 4;
+    console.log("Secondary Partial or Full Hour Fee: +4");
+}
+
+// for live timestamp
+const calculateCost = (time) => {
+    addEntranceFee();
+    console.log(cost);
+    while(time != 0){
+        if(time <= 1){
+            addPartialHrFee();
+        }
+        else{
+            addFullHrFee();
+        }
+        time = time - 1;
+    }
+    console.log("Total cost for parking is: ",cost);
+    document.getElementById('cost').innerText = '$' + cost;
+    cost = 0;
+}
+
+
+
+// for manual entry time
+function calculateTime(e,l){
+    var hours = Number(l.split(':')[0]) - Number(e.split(':')[0]);
+    var minutes = Number(l.split(':')[1]) - Number(e.split(':')[1]);
+    // condition for - ve hours
+    if(hours < 0){
+        hours = 24 - Number(l.split(':')[0]);
+        hours = hours + Number(e.split(':')[0]);
+    }
+    if(minutes < 0){
+        minutes = 60 - Number(l.split(':')[1]);
+        minutes = minutes + Number(e.split(':')[1]);
+    }
+    // condition for - ve minutes
+    if(minutes > 30){
+        // count partial hour
+        hours = hours + 1;
+    }
+    var time = hours;
+    console.log("time:", time);
+    calculateCost(time);
+}
+
+document.getElementById('calculate').addEventListener('click',() =>{
+    var entry = document.getElementById('entrytime').value;
+    var leave = document.getElementById('leavetime').value;
+    console.log(entry,leave);
+    calculateTime(entry,leave);
+})
+
+const init = () => {
+    // var time = leaveTime - entryTime; //calculate no. of hours
+    var hours = Number(leaveTime.split(':')[0]) - Number(entryTime.split(':')[0]);
+    var minutes = Number(leaveTime.split(':')[1]) - Number(entryTime.split(':')[1]);
+    // condition for - ve hours
+    if(hours < 0){
+        hours = 24 - Number(leaveTime.split(':')[0]);
+        hours = hours + Number(entryTime.split(':')[0]);
+    }
+    if(minutes < 0){
+        minutes = 60 - Number(leaveTime.split(':')[1]);
+        minutes = minutes + Number(entryTime.split(':')[1]);
+    }
+    // condition for - ve minutes
+    if(minutes > 30){
+        // count partial hour
+        hours = hours + 1;
+    }
+    var time = hours;
+    calculateCost(time);
+}   
+
+function enter(){
+    var hr = new Date().getHours();
+    var min = new Date().getMinutes();
+    console.log("Entry time now: " + hr + ':' + min);
+    entryTime = hr + ":" + min;
+    document.getElementById('entryTime').innerText = "Entry timestamp (HH:MM): " + entryTime;
+}
+
+function leave(){
+    var hr = new Date().getHours() ;
+    var min = new Date().getMinutes() ;
+    console.log("Leave time now: " + hr + ':' + min);
+    leaveTime = hr + ":" + min;
+    document.getElementById('leaveTime').innerText = "Leave timestamp (HH:MM): " +  leaveTime;
+    init();
+}
